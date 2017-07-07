5df4a661def7e0e32c049fae12ede9085bed76bb
ddopik <ddopik.01@gmail.com>
2017-07-07 11:36:26 +0200

    Saving 7/7 11.36

--- app/src/main/java/com/codecourt/ddopikmain/seedapplication/app/View/SplashScreen.java
+++ app/src/main/java/com/codecourt/ddopikmain/seedapplication/app/View/SplashScreen.java
@@ -36,8 +36,8 @@ public class SplashScreen extends AppCompatActivity {
 
     public void splashStart() {
         mprogressBar = (ProgressBar) findViewById(R.id.circular_progress_bar);
-        ObjectAnimator anim = ObjectAnimator.ofInt(mprogressBar, "progress", 0, 31);
-        anim.setDuration(20000);
+        ObjectAnimator anim = ObjectAnimator.ofInt(mprogressBar, "progress", 0,23);
+        anim.setDuration(15000);
         anim.setInterpolator(new DecelerateInterpolator());
         anim.start();
 
